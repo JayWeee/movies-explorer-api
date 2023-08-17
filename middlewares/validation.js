1a6ed1360a6e9e881d5e67c8b3cafe762d558902
JayWee <tylykin2013@yandex.ru>
2023-08-17 12:11:18 +0300

    Сделать поля name и email обязательными при валидации обновления пользователя. Добавить ограничение длинны пароля при валидации авторизации

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,8 +4,8 @@ const regExUrl = /^https?:\/\/(?:w{3}\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-
 
 const validateUpdateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().email(),
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
   }),
 });
 
@@ -20,7 +20,7 @@ const validateCreateUser = celebrate({
 const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    password: Joi.string().required().min(8),
   }),
 });
 
